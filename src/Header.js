73a870f75a15aab03db1de1d4657a1ed006f4930
mplusm <73212622+mplusm@users.noreply.github.com>
2021-05-21 01:20:14 -0700

    first commit

--- src/Header.js
+++ src/Header.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
 
 
 
@@ -35,25 +36,21 @@ function Header() {
                 </div>
                 <div 
                 className="header__option">
-                        <span className = "header__optionLineOne">
-                        Returns
-                    </span>
+                        <span className = "header__optionLineOne"> Returns </span>
 
-                    <span className = "header__optionLineTwo">
-                        & Orders
-                    </span>
-
-                </div>
+                        <span className = "header__optionLineTwo"> & Orders </span>
+                    </div>
                 <div 
                 className="header__option">
-                    <span className = "header__optionLineOne">
-                        Your
-                    </span>
+                    <span className = "header__optionLineOne"> Your </span>
 
-                    <span className = "header__optionLineTwo">
-                        Prime
-                    </span>
+                    <span className = "header__optionLineTwo"> Prime </span>
                 </div>
+              <div className="header__optionBasket"> 
+              <ShoppingCartIcon />
+              <span className = "header__optionLineTwo header__basketCount">0</span>
+
+              </div>
 
             </div>
 
