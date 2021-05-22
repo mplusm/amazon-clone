095508f8fac2b20360c6839690c29e20d148a99b
mplusm <73212622+mplusm@users.noreply.github.com>
2021-05-22 00:14:01 -0700

    react-router

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@ import React from "react";
 import './App.css';
 import Header from './Header'
 import Home from './Home';
+import { BrowserRouter as Router, Switch, Route}
+from "react-router-dom";
 
 
 
 function App() {
   return (
+    <Router>
     <div className="app">
-      <Header /> 
-      <Home/>
+      <Switch>
+        <Route path="/checkout"> 
+        <Header />
+        <h1> checkout page</h1>
+        </Route>
+      <Route path="/">
+        <Header/>
+        <Home/>
+      </Route>
+      </Switch>
+      
     </div>
+    </Router>
   );
 }
 
