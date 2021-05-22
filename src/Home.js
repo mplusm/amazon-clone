22012b09dba153d00eb183cc9e3395970d5eb397
mplusm <73212622+mplusm@users.noreply.github.com>
2021-05-21 23:21:53 -0700

    header

--- src/Home.js
+++ src/Home.js
@@ -11,18 +11,33 @@ function Home() {
                 alt=""/>
 
                 <div className="home__row">
+
+                    <Product title='the lean startup' price={29.99}
+                    image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._AC_SY400_.jpg"
+                    rating ={5}
+                    />
+
+                    <Product
+                    title ="Kenwood Mix Stand for Baking"
+                    price={239.0}
+                    rating={4}
+                    image=""
+                    />
+
+                </div>
+
+                <div className="home__row">
                     <Product/>
+                    <Product/>
+                    <Product/>
+                    
                     
                 </div>
 
                 <div className="home__row">
-                    {/*product*/}
-                    {/*product*/}
-                </div>
-
-                <div className="home__row">
-                    {/*product*/}
-                </div>
+                    <Product/>
+                    
+                    </div>
             </div>
             
         </div>
