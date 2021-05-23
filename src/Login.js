23d31359b9ff7811d5fc551e1cb62c66e3c2e1ff
mplusm <73212622+mplusm@users.noreply.github.com>
2021-05-23 00:57:31 -0700

    auth

--- src/Login.js
+++ src/Login.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import './Login.css';
+import './Login.css'
 import { Link, useHistory } from "react-router-dom";
 import { auth } from "./firebase";
-
 
 function Login() {
     const history = useHistory();
@@ -10,57 +9,61 @@ function Login() {
     const [password, setPassword] = useState('');
 
     const signIn = e => {
-        e.preventDefault ();
+        e.preventDefault();
 
-        auth.signInWithEmailAndPassword(email, password).then
-        (auth => {
-            history.push('/')
-        })
-        .catch(error => alert(error.message))
-        //firebase login 
+        auth
+            .signInWithEmailAndPassword(email, password)
+            .then(auth => {
+                history.push('/')
+            })
+            .catch(error => alert(error.message))
     }
+
     const register = e => {
         e.preventDefault();
 
-        auth.createUserWithEmailAndPassword(email, password)
-        .then((auth) =>  {
-        if (auth) {
-            history.push('/')
-        }
-    })
-
-    .catch(error => alert(error.message))
+        auth
+            .createUserWithEmailAndPassword(email, password)
+            .then((auth) => {
+                // it successfully created a new user with email and password
+                if (auth) {
+                    history.push('/')
+                }
+            })
+            .catch(error => alert(error.message))
+    }
 
     return (
         <div className='login'>
-            <Link to ='/'>
-            <img 
-            className="login__logo"
-            src="https://www.gannett-cdn.com/-mm-/8eded6e5fecea21a0098fa1a295b13d637260962/c=322-342-1887-1226&r=x1683&c=3200x1680/local/-/media/2017/09/07/Rochester/Rochester/636403844012827674-Amazon-logo.png"/>
-
-
+            <Link to='/'>
+                <img
+                    className="login__logo"
+                    src='https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png' 
+                />
             </Link>
-            <div className="login__container">
-                <h1>Sign In</h1>
-                <form>
-                  <h5>E-mail</h5>
-                  <input type='text' value={email} onChange={e => setEmail(e.target.value)}/>
 
-                  <h5>Password</h5>
-                  <input type='password' value={password} 
-                  onChange={e => setPassword(e.target.value)}
-                    />
-         
-                  <button type='submit' onClick={signIn} className='login__signInButton'> Sign In</button>
+            <div className='login__container'>
+                <h1>Sign-in</h1>
+
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)} />
+
+                    <h5>Password</h5>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)} />
+
+                    <button type='submit' onClick={signIn} className='login__signInButton'>Sign In</button>
                 </form>
-                
+
                 <p>
-                    By Signing you agree to Amazon-clone conditions built by Mallesh Madapathi (developerr.io)
+                    Accept terms & conditions by signin-in
+                    website built by <strong>Mallesh Madapathi (https://developerr.io)</strong>
                 </p>
-                <button onClick={register} className='login__registerButton'> Create An Account</button>
+
+                <button onClick={register} className='login__registerButton'>Create your Amazon Account</button>
             </div>
         </div>
     )
 }
 
-export default Login;
+export default Login
